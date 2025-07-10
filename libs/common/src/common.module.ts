6401c40fab593be9c9fe231dead474180ec47b97
Vahid Akhtar <akhtarvahid543@gmail.com>
2025-07-11 00:36:40 +0530

    feat: db and configs module setup

--- libs/common/src/common.module.ts
+++ libs/common/src/common.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CommonService } from './common.service';
+import { DatabaseModule } from './database/database.module';
+import { ConfigsModule } from './configs/configs.module';
 
 @Module({
   providers: [CommonService],
   exports: [CommonService],
+  imports: [DatabaseModule, ConfigsModule],
 })
 export class CommonModule {}
