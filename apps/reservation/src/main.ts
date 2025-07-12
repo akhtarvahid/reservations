5e132463008644ea244732b61014839a7ec74533
Vahid Akhtar <akhtarvahid543@gmail.com>
2025-07-12 14:39:50 +0530

    feat: pino packages for logging

--- apps/reservation/src/main.ts
+++ apps/reservation/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationModule } from './reservation.module';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ReservationModule);
-  await app.listen(process.env.port ?? 3000);
+ const app = await NestFactory.create(ReservationModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
+  const configService = app.get(ConfigService);
+  await app.listen(configService.get('PORT') || 3000);
 }
 bootstrap();
