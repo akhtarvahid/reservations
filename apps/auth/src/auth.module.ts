e800f130523a349349bd96342f840ff60b6ee57c
Vahid Akhtar <akhtarvahid543@gmail.com>
2025-07-12 20:07:27 +0530

    feat: config module changed, jwt module setup

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -4,10 +4,33 @@ import { AuthService } from './auth.service';
 import { UsersModule } from './users/users.module';
 import { UsersController } from './users/users.controller';
 import { LoggerModule } from '@app/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [UsersModule, LoggerModule],
-  controllers: [AuthController, UsersController],
+  imports: [
+    UsersModule,
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRATION: Joi.string().required(),
+      }),
+    }),
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: `${configService.get('JWT_EXPIRATION')}s`,
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AuthController],
   providers: [AuthService],
 })
 export class AuthModule {}
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
-import { ConfigsModule } from '../configs/configs.module';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      imports: [ConfigsModule],
+    
       useFactory: (configService: ConfigService) => ({
         uri: configService.get('MONGO_URI'),
       }),
