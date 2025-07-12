c9b7392a7241379c712efe742941cf99069ac50b
Vahid Akhtar <akhtarvahid543@gmail.com>
2025-07-12 15:36:51 +0530

    dockerized

--- apps/reservation/src/main.ts
+++ apps/reservation/src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
-  await app.listen(configService.get('PORT') || 3000);
+  await app.listen(configService.get('APP_PORT') || 3000);
+  console.log(`App is running at http://localhost:${configService.get('APP_PORT') | 3000}`);
 }
 bootstrap();
